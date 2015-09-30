19ea7a0074ce04523e72f03fd312400ae7de4337
vsilent <jabberroid@gmail.com>
2015-09-30 15:39:36 +0300

    Vagrant file added, provision script created

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# Vision project
+# Vision
+
 
 # Installation
+
+Ubuntu Linux
+
+1. $ sudo apt-get install virtualbox vagrant
+2. $ sudo bash
+3. $ echo "192.168.88.88 dev.vision.local" >> /etc/hosts
+4. $ exit
+5. $ cd yourprojectsdir
+6. $ mkdir vision && cd vision
+7. $ git clone https://github.com/SnowBeaver/Vision.git project
+8. $ cd project
+9. $ vagrant up
---
+++ provision.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd /home/vision/www
+
+# recreate database and load clean dump
+echo "Stopping all services"
+sudo supervisorctl stop all
+
+# recreate database and load clean dump
+echo "Installing database:"
+sudo service postgresql restart
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'ViSiOn'";
+PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "DROP DATABASE IF EXISTS vision";
+PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "DROP ROLE IF EXISTS vision";
+PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "CREATE ROLE vision WITH LOGIN PASSWORD 'ViSiOn'";
+PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "ALTER ROLE vision WITH CREATEDB";
+PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "CREATE DATABASE vision OWNER vision";
+
+# create structure and fill fixtures
+
+# clear the cache
+
+# reload supervisor to start all the services
+echo "Starting all services"
+sudo supervisorctl restart all
+
+# remove all the logs stored in project directory
