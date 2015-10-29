83cab4ec1aeda1dc3ae319029b41e3a62d431865
optimum-web <dumitru.esanu@laptop758.intern.arobs.com>
2015-10-29 14:54:49 +0200

    verify if user is different than none

--- app/home/models.py
+++ app/home/models.py
@@ -1,8 +1,12 @@
 # check if current user has write options
 def _is_blogger(current_user,blogger_permission):
-    authenticated = current_user.is_authenticated() if \
-        callable(current_user.is_authenticated) \
-        else current_user.is_authenticated
+    if current_user is not None:
+	    authenticated = current_user.is_authenticated() if \
+	        callable(current_user.is_authenticated) \
+	        else current_user.is_authenticated
+    else:
+    	authenticated = False
+
     is_blogger = authenticated and \
         blogger_permission.require().can()
     return is_blogger
