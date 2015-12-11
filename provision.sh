752dd6a64500896013b2aefe6455d06a898652ec
vsilent <jabberroid@gmail.com>
2015-12-11 09:22:09 +0200

    provisioning update

--- provision.sh
+++ provision.sh
@@ -13,17 +13,12 @@ sudo service supervisor stop
 echo "Installing database:"
 sudo service postgresql restart
 
-#sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'ViSiOn'";
-#PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "DROP DATABASE IF EXISTS vision";
-#PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "DROP ROLE IF EXISTS vision";
-#PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "CREATE ROLE vision WITH LOGIN PASSWORD 'ViSiOn'";
-#PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "ALTER ROLE vision WITH CREATEDB";
-#PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "CREATE DATABASE vision OWNER vision";
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'ViSiOn'";
+PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "DROP DATABASE IF EXISTS vision";
+PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "DROP ROLE IF EXISTS vision";
+PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "CREATE ROLE vision WITH LOGIN PASSWORD 'ViSiOn'";
+PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "ALTER ROLE vision WITH CREATEDB";
+PGPASSWORD="ViSiOn" psql -U postgres -p 5432 -h localhost -c "CREATE DATABASE vision OWNER vision";
 
 
-# create structure and fill fixtures
-# clear the cache
-# reload supervisor to start all the services
-# remove all the logs stored in project directory
-
-fab setup_dev --password vagrant
+# complete provisioning using fabric on the host machine: fab setup_dev --password vagrant
