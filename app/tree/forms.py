d9c766cdbbe1e0107a1bc979bce6cf9ec3132a82
vsilent <jabberroid@gmail.com>
2016-03-29 14:58:01 +0300

    PEP8 formatting

--- app/tree/forms.py
+++ app/tree/forms.py
@@ -1,12 +1,11 @@
 from flask.ext.wtf import Form
-from wtforms import SelectField , HiddenField , TextField
-from wtforms.validators import Required , Length
+from wtforms import SelectField, HiddenField, TextField
+from wtforms.validators import Required, Length
 
-myChoices = [('home', 'Home'), ('form','Form') , ('text','Text') ]
+myChoices = [('home', 'Home'), ('form', 'Form'), ('text', 'Text')]
+
 
 class TreeView(Form):
-    view = SelectField(u'Select View', choices = myChoices, validators = [Required()])
-    tooltip = TextField(u'Update Tooltip', validators = [Required() , Length(min=1, max=512)])
-    node_id = HiddenField("tree_id" , validators = [Required()])
-
-
+    view = SelectField(u'Select View', choices=myChoices, validators=[Required()])
+    tooltip = TextField(u'Update Tooltip', validators=[Required(), Length(min=1, max=512)])
+    node_id = HiddenField("tree_id", validators=[Required()])
