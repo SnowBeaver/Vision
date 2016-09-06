b18f974d46df21fe9de1669a29a914708ce5227f
dmitry <dmitry.j@optimum-web.com>
2016-09-06 18:24:47 +0300

    Add inflating and saving abilities to NewInhibitorTestForm.

--- app/static/src/diagnostic/TestTypeResultForm_modules/NewInhibitorTestForm.js
+++ app/static/src/diagnostic/TestTypeResultForm_modules/NewInhibitorTestForm.js
@@ -10,15 +10,11 @@ import { hashHistory } from 'react-router';
 import {Link} from 'react-router';
 
 
-var items=[];
-
 const TextField = React.createClass({
     render: function() {
         var label = (this.props.label != null) ? this.props.label: "";
         var name = (this.props.name != null) ? this.props.name: "";
         var value = (this.props.value != null) ? this.props.value: "";
-        console.log("NewFluidTestForm TextField " + name + " value: " + value);
-        console.log("NewFluidTestForm TextField " + name + " props.value: " + this.props.value);
         return (
             <FormGroup>
                 <ControlLabel>{label}</ControlLabel>
@@ -36,18 +32,30 @@ const TextField = React.createClass({
 const CheckBox = React.createClass({
     render: function () {
         var name = (this.props.name != null) ? this.props.name: "";
-        return (
-            <Checkbox name={name}>
-                <span className="glyphicon glyphicon-menu-left" >
-                </span>
-            </Checkbox>
-        );
+        var checked = (this.props.value != null) ? this.props.value: false;
+        if (checked) {
+            return (
+                <Checkbox checked name={name}>
+                    <span className="glyphicon glyphicon-menu-left">
+                    </span>
+                </Checkbox>
+            );
+        }
+        else {
+            return (
+                <Checkbox name={name}>
+                    <span className="glyphicon glyphicon-menu-left" >
+                    </span>
+                </Checkbox>
+            );
+        }
     }
 
 });
 
 var SelectField = React.createClass({
     handleChange: function(event, index, value){
+        console.log("Handle change");
         this.setState({
             value: event.target.value
         });
@@ -63,7 +71,8 @@ var SelectField = React.createClass({
         return this.state.isVisible;
     },
     componentDidMount: function(){
-        this.serverRequest = $.get(this.props.source, function (result){
+        var source = '/api/v1.0/' + this.props.source + '/';
+        this.serverRequest = $.get(source, function (result){
             this.setState({ items: (result['result']) });
         }.bind(this), 'json');
     },
@@ -74,89 +83,26 @@ var SelectField = React.createClass({
         this.state.isVisible = true;
     },
     render: function() {
+        var label = (this.props.label != null) ? this.props.label: "";
+        var name = (this.props.name != null) ? this.props.name: "";
+        var value = (this.props.value != null) ? this.props.value: "";
         var menuItems = [];
         for (var key in this.state.items) {
             menuItems.push(<option key={this.state.items[key].id}
                                    value={this.state.items[key].id}>{`${this.state.items[key].name}`}</option>);
         }
-        console.log( "SelectField value" + (this.props.value || 'no data') );
-        console.log( this.props.value );
-        console.log( typeof(this.state.value) == "undefined" );
-        console.log( this.state.value == null );
         return (
             <FormGroup>
+                <ControlLabel>{label}</ControlLabel>
                 <FormControl componentClass="select"
                              onChange={this.handleChange}
-                             defaultValue={this.props.value}
+                             name={name}
+                             value={value}
                              >
-                    <option>{this.props.label}</option>
                     {menuItems}
+                    <FormControl.Feedback />
                 </FormControl>
             </FormGroup>
-        );
-    }
-});
-
-var InhibitorTypeSelectField = React.createClass ({
-
-    handleChange: function(event, index, value){
-        this.setState({
-            value: event.target.value,
-            location_id: event.target.value
-        })
-
-    },
-
-    getInitialState: function(){
-        return {
-            items: [],
-            isVisible: false
-        };
-    },
-
-    isVisible: function(){
-        return this.state.isVisible;
-    },
-
-    componentDidMount: function(){
-        this.serverRequest = $.get(this.props.source, function (result){
-
-            items = (result['result']);
-            this.setState({
-                items: items
-            });
-        }.bind(this), 'json');
-    },
-
-    componentWillUnmount: function() {
-        this.serverRequest.abort();
-    },
-
-    setVisible: function(){
-        this.state.isVisible = true;
-    },
-
-    render: function() {
-        var menuItems = [];
-        for (var key in this.state.items) {
-            menuItems.push(<option key={this.state.items[key].id} value={this.state.items[key].id}>{`${this.state.items[key].name}`}</option>);
-        }
-
-        return (
-            <div>
-                <FormGroup>
-                    <FormControl
-                        componentClass="select"
-                        placeholder="select"
-                        onChange={this.handleChange}
-                        name="inhibitor_type_id"
-                        value={this.state.selected}
-                    >
-                        <option value="select">Inhibitor Type</option>
-                        {menuItems}
-                    </FormControl>
-                </FormGroup>
-            </div>
         );
     }
 });
@@ -172,23 +118,46 @@ var NewInhibitorTestForm = React.createClass({
                 'inhibitor_flag',
                 'inhibitor_type_id',
                 'remark'
-
-
             ]
         }
     },
 
+    componentDidMount: function () {
+        var source = '/api/v1.0/' + this.props.tableName + '/?test_result_id=' + this.props.testResultId;
+        this.serverRequest = $.get(source, function (result) {
+            var res = (result['result']);
+            if (res.length > 0) {
+                var fields = this.state.fields;
+                fields.push('id');
+                var data = res[0];
+                var state = {};
+                for (var i = 0; i < fields.length; i++) {
+                    var key = fields[i];
+                    if (data.hasOwnProperty(key)) {
+                        state[key] = data[key];
+                    }
+                }
+                this.setState(state);
+            }
+        }.bind(this), 'json');
+    },
+
     _create: function () {
         var fields = this.state.fields;
-        var data = {};
+        var data = {test_result_id: this.props.testResultId};
+        var url = '/api/v1.0/' + this.props.tableName + '/';
+        var type = 'POST';
         for (var i = 0; i < fields.length; i++) {
             var key = fields[i];
             data[key] = this.state[key];
         }
-
+        if ('id' in this.state) {
+            url += this.state['id'];
+            type = 'PUT';
+        }
         return $.ajax({
-            url: '/api/v1.0/inhibitor_test/',
-            type: 'POST',
+            url: url,
+            type: type,
             dataType: 'json',
             contentType: 'application/json',
             data: JSON.stringify(data),
@@ -237,94 +206,105 @@ var NewInhibitorTestForm = React.createClass({
     },
 
     _onChange: function (e) {
-        var state = {};
-        if (e.target.type == 'select-one') {
-            state[e.target.name] = e.target.value;}
-        else if (e.target.type == 'checkbox') {
-            state[e.target.name] = e.target.checked;
-        }
-        else{state[e.target.name] = $.trim(e.target.value);}
-        this.setState(state);
+       var state = {};
+       if (e.target.type == 'checkbox') {
+           state[e.target.name] = e.target.checked;
+       }
+       else if (e.target.type == 'radio') {
+           state[e.target.name] = e.target.value;
+       }
+       else if (e.target.type == 'select-one') {
+           state[e.target.name] = e.target.value;
+       }
+       else {
+           state[e.target.name] = $.trim(e.target.value);
+       }
+       this.setState(state);
     },
 
-        _validate: function () {
-            var errors = {};
-            // if(this.state.created_by_id == "") {
-            //   errors.created_by_id = "Create by field is required";
-            // }
-            // if(this.state.performed_by_id == "") {
-            //     errors.performed_by_id = "Performed by field is required";
-            // }
-            return errors;
-        },
+    _validate: function () {
+        var errors = {};
+        // if(this.state.created_by_id == "") {
+        //   errors.created_by_id = "Create by field is required";
+        // }
+        // if(this.state.performed_by_id == "") {
+        //     errors.performed_by_id = "Performed by field is required";
+        // }
+        return errors;
+    },
 
-        _formGroupClass: function (field) {
-            var className = "form-group ";
-            if (field) {
-                className += " has-error"
-            }
-            return className;
-        },
+    _formGroupClass: function (field) {
+        var className = "form-group ";
+        if (field) {
+            className += " has-error"
+        }
+        return className;
+    },
 
-        render: function () {
-
-            return (
-                <div className="form-container">
-                    <form method="post" action="#" onSubmit={this._onSubmit} onChange={this._onChange}>
-                        <div className="row">
-                            <div className="col-md-8">
-                                <div className="row">
-                                    <div className="col-xs-1 ">
-                                        <CheckBox name="inhibitor_flag"/>
-                                    </div>
-                                    <div className="col-xs-6">
-                                        <TextField label="Inhibitor" name="inhibitor" value={this.state.inhibitor}/>
-                                    </div>
+    render: function () {
+        return (
+            <div className="form-container">
+                <form method="post" action="#" onSubmit={this._onSubmit} onChange={this._onChange}>
+                    <div className="row">
+                        <div className="col-md-8">
+                            <div className="row">
+                                <div className="col-xs-1 ">
+                                    <CheckBox name="inhibitor_flag" value={this.state.inhibitor_flag}/>
                                 </div>
-                                <div className="row">
-                                    <div>
-                                        <SelectField source="" label="Inhibitor Type" value=""/>
-                                    </div>
+                                <div className="col-xs-6">
+                                    <TextField label="Inhibitor" name="inhibitor" value={this.state.inhibitor}/>
                                 </div>
-                                <div className="row">
+                            </div>
+                            <div className="row">
+                                <div className="col-xs-7">
+                                    <SelectField label="Inhibitor Type"
+                                                 name="inhibitor_type_id"
+                                                 value={this.state.inhibitor_type_id}
+                                                 source="inhibitor_type"/>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-xs-7">
                                     <FormGroup>
                                         <FormControl componentClass="textarea"
                                                      placeholder="Remark"
                                                      name="remark"
+                                                     value={this.state.remark}
                                         />
+                                        <FormControl.Feedback />
                                     </FormGroup>
                                 </div>
                             </div>
-
-                            <div className="col-md-4">
-                                <Panel header="Calculation of inhibitor quantity to add to the transformer">
-                                    <TextField label="Final inhibitor concentration(ppm)" name="" value=""/>
-                                    <TextField label="Aditive mixture concetration(% v/v)" name="" value=""/>
-                                    <TextField label="Required quantity of additive(liters)" disabled name="" value=""/>
-                                    <TextField label="Required dry crystal weight(kg)" disabled name="" value=""/>
-                                    <TextField label="Equipment oil volume(liters)" disabled name="" value=""/>
-                                </Panel>
-                            </div>
                         </div>
 
-                        <div className="row">
-                            <div className="col-md-12 ">
-                                <Button bsStyle="success"
-                                        className="pull-right"
-                                        onClick={this.props.handleClose}
-                                        type="submit">Save</Button>
-                                &nbsp;
-                                <Button bsStyle="danger"
-                                        className="pull-right margin-right-xs"
-                                        onClick={this.props.handleClose}
-                                >Cancel</Button>
-                            </div>
+                        <div className="col-md-4">
+                            <Panel header="Calculation of inhibitor quantity to add to the transformer">
+                                <TextField label="Final inhibitor concentration(ppm)" name="" value=""/>
+                                <TextField label="Aditive mixture concetration(% v/v)" name="" value=""/>
+                                <TextField label="Required quantity of additive(liters)" disabled name="" value=""/>
+                                <TextField label="Required dry crystal weight(kg)" disabled name="" value=""/>
+                                <TextField label="Equipment oil volume(liters)" disabled name="" value=""/>
+                            </Panel>
                         </div>
-                    </form>
-                </div>
-            );
-        }
-    });
+                    </div>
 
+                    <div className="row">
+                        <div className="col-md-12 ">
+                            <Button bsStyle="success"
+                                    className="pull-right"
+                                    onClick={this.props.handleClose}
+                                    type="submit">Save</Button>
+                            &nbsp;
+                            <Button bsStyle="danger"
+                                    className="pull-right margin-right-xs"
+                                    onClick={this.props.handleClose}
+                            >Cancel</Button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+});
 
 export default NewInhibitorTestForm;
