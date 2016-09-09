f14c6ed82a554088e2c896521bbf100cc0bf725d
Anna Corobco <anna@optimum-web.com>
2016-09-09 17:19:44 +0300

    Show all the errors returned by API on adding manufacturer. Mark required fields. Corrected typo in the word 'addded' in NewEquipmentTypeForm

--- app/static/src/diagnostic/EquipmentForm_modules/NewManufacturerForm.js
+++ app/static/src/diagnostic/EquipmentForm_modules/NewManufacturerForm.js
@@ -6,6 +6,8 @@ import Button from 'react-bootstrap/lib/Button';
 import Panel from 'react-bootstrap/lib/Panel';
 import {findDOMNode} from 'react-dom';
 import { hashHistory } from 'react-router';
+import HelpBlock from 'react-bootstrap/lib/HelpBlock';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 
 
 
@@ -20,12 +22,13 @@ var NewManufacturerForm = React.createClass({
                 'markings',
                 'location',
                 'description'
-            ]
+            ],
+            changedFields: []
         }
     },
 
     _create: function () {
-        var fields = this.state.fields;
+        var fields = this.state.changedFields;
         var data = {};
         for (var i = 0; i < fields.length; i++) {
             var key = fields[i];
@@ -46,12 +49,9 @@ var NewManufacturerForm = React.createClass({
 
     _onSubmit: function (e) {
         e.preventDefault();
-        var errors = this._validate();
-        if (Object.keys(errors).length != 0) {
-            this.setState({
-                errors: errors
-            });
-            return;
+        if (!this._validate()){
+            NotificationManager.error('Please correct the errors');
+            return false;
         }
         var xhr = this._create();
         xhr.done(this._onSuccess)
@@ -64,8 +64,7 @@ var NewManufacturerForm = React.createClass({
     },
 
     _onSuccess: function (data) {
-        // this.setState(this.getInitialState());
-
+        NotificationManager.success("Manufacturer added.");
     },
 
     _onError: function (data) {
@@ -76,27 +75,40 @@ var NewManufacturerForm = React.createClass({
             message = data.responseJSON.message;
         }
         if (res.error) {
-            this.setState({
-                errors: res.error
-            });
+            // Join multiple error messages
+            if (res.error instanceof Object){
+                for (var field in res.error) {
+                    var errorMessage = res.error[field];
+                    if (Array.isArray(errorMessage)) {
+                        errorMessage = errorMessage.join(". ");
+                    }
+                    res.error[field] = errorMessage;
+                }
+                this.setState({
+                    errors: res.error
+                });
+            } else {
+                message = res.error;
+            }
         }
+        NotificationManager.error(message);
     },
 
     _onChange: function (e) {
         var state = {};
             state[e.target.name] = $.trim(e.target.value);
+        state.changedFields = this.state.changedFields.concat([e.target.name]);
+        state.errors = this.state.errors;
+        delete state.errors[e.target.name];
         this.setState(state);
     },
 
     _validate: function () {
-        var errors = {};
-        // if(this.state.created_by_id == "") {
-        //   errors.created_by_id = "Create by field is required";
-        // }
-        // if(this.state.performed_by_id == "") {
-        //     errors.performed_by_id = "Performed by field is required";
-        // }
-        return errors;
+        var response = true;
+        if (Object.keys(this.state.errors).length > 0){
+            response = false;
+        }
+        return response;
     },
 
     _formGroupClass: function (field) {
@@ -118,10 +130,12 @@ var NewManufacturerForm = React.createClass({
                     <form method="post" action="#" onSubmit={this._onSubmit} onChange={this._onChange}>
                         <div className="row">
                             <div className="col-md-12">
-                                <FormGroup>
+                                <FormGroup validationState={this.state.errors.name ? 'error' : null}>
+                                    <HelpBlock className="warning">{this.state.errors.name}</HelpBlock>
                                     <FormControl type="text"
-                                                 placeholder="Name"
+                                                 placeholder="Name *"
                                                  name="name"
+                                                 required
                                     />
                                 </FormGroup>
                             </div>
@@ -129,7 +143,8 @@ var NewManufacturerForm = React.createClass({
 
                             <div className="row">
                                 <div className="col-md-12">
-                                    <FormGroup>
+                                    <FormGroup validationState={this.state.errors.markings ? 'error' : null}>
+                                        <HelpBlock className="warning">{this.state.errors.markings}</HelpBlock>
                                         <FormControl componentClass="textarea"
                                                      placeholder="Markings"
                                                      name="markings"/>
@@ -139,7 +154,8 @@ var NewManufacturerForm = React.createClass({
 
                         <div className="row">
                             <div className="col-md-12">
-                                <FormGroup>
+                                <FormGroup validationState={this.state.errors.location ? 'error' : null}>
+                                    <HelpBlock className="warning">{this.state.errors.location}</HelpBlock>
                                     <FormControl type="text"
                                                  placeholder="Location"
                                                  name="location"
@@ -150,7 +166,8 @@ var NewManufacturerForm = React.createClass({
 
                             <div className="row">
                                 <div className="col-md-12">
-                                    <FormGroup>
+                                    <FormGroup validationState={this.state.errors.description ? 'error' : null}>
+                                        <HelpBlock className="warning">{this.state.errors.description}</HelpBlock>
                                         <FormControl componentClass="textarea"
                                                      placeholder="Description"
                                                      name="description"/>
