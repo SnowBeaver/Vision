9fd6ebe6c39c78fb79d809158e2d1643c1a2c39f
Anna Corobco <anna@optimum-web.com>
2016-09-09 17:50:49 +0300

    Show all errors returned from API on creating new location. Show nice notifications

--- app/static/src/diagnostic/EquipmentForm_modules/NewLocationForm.js
+++ app/static/src/diagnostic/EquipmentForm_modules/NewLocationForm.js
@@ -7,6 +7,8 @@ import Panel from 'react-bootstrap/lib/Panel';
 import {findDOMNode} from 'react-dom';
 import { hashHistory } from 'react-router';
 import {Link} from 'react-router';
+import HelpBlock from 'react-bootstrap/lib/HelpBlock';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 
 
 var NewLocationForm = React.createClass({
@@ -17,12 +19,18 @@ var NewLocationForm = React.createClass({
             errors: {},
             fields: [
                 'name'
-            ]
+            ],
+            changedFields: []
         }
     },
 
     _create: function () {
-        var fields = this.state.fields;
+        var fields = this.state.changedFields;
+        if (fields.length == 0){
+            NotificationManager.info("No values were entered.");
+            return false;
+        }
+
         var data = {};
         for (var i = 0; i < fields.length; i++) {
             var key = fields[i];
@@ -50,10 +58,13 @@ var NewLocationForm = React.createClass({
             });
             return;
         }
+
         var xhr = this._create();
-        xhr.done(this._onSuccess)
-            .fail(this._onError)
-            .always(this.hideLoading)
+        if (xhr){
+            xhr.done(this._onSuccess)
+                .fail(this._onError)
+                .always(this.hideLoading);
+        }
     },
 
     hideLoading: function () {
@@ -62,7 +73,7 @@ var NewLocationForm = React.createClass({
 
     _onSuccess: function (data) {
         // this.setState(this.getInitialState());
-
+        NotificationManager.success("Location added.");
     },
 
     _onError: function (data) {
@@ -73,27 +84,40 @@ var NewLocationForm = React.createClass({
             message = data.responseJSON.message;
         }
         if (res.error) {
-            this.setState({
-                errors: res.error
-            });
+            // Join multiple error messages
+            if (res.error instanceof Object){
+                for (var field in res.error) {
+                    var errorMessage = res.error[field];
+                    if (Array.isArray(errorMessage)) {
+                        errorMessage = errorMessage.join(". ");
+                    }
+                    res.error[field] = errorMessage;
+                }
+                this.setState({
+                    errors: res.error
+                });
+            } else {
+                message = res.error;
+            }
         }
+        NotificationManager.error(message);
     },
 
     _onChange: function (e) {
         var state = {};
             state[e.target.name] = $.trim(e.target.value);
+        state.changedFields = this.state.changedFields.concat([e.target.name]);
+        state.errors = this.state.errors;
+        delete state.errors[e.target.name];
         this.setState(state);
     },
 
     _validate: function () {
-        var errors = {};
-        // if(this.state.created_by_id == "") {
-        //   errors.created_by_id = "Create by field is required";
-        // }
-        // if(this.state.performed_by_id == "") {
-        //     errors.performed_by_id = "Performed by field is required";
-        // }
-        return errors;
+        var response = true;
+        if (Object.keys(this.state.errors).length > 0){
+            response = false;
+        }
+        return response;
     },
 
     _formGroupClass: function (field) {
@@ -111,7 +135,8 @@ var NewLocationForm = React.createClass({
                     <form method="post" action="#" onSubmit={this._onSubmit} onChange={this._onChange}>
                         <div className="row">
                             <div className="col-md-12">
-                                <FormGroup>
+                                <FormGroup validationState={this.state.errors.name ? 'error' : null}>
+                                    <HelpBlock className="warning">{this.state.errors.name}</HelpBlock>
                                     <FormControl type="text"
                                                  placeholder="Name"
                                                  name="name"
