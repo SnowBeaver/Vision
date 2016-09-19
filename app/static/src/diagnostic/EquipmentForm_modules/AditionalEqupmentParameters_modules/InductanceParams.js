c029bab611987547cf2b0415896b7749b86885c7
Anna Corobco <anna@optimum-web.com>
2016-09-19 11:31:52 +0300

    Validating subforms

--- app/static/src/diagnostic/EquipmentForm_modules/AditionalEqupmentParameters_modules/InductanceParams.js
+++ app/static/src/diagnostic/EquipmentForm_modules/AditionalEqupmentParameters_modules/InductanceParams.js
@@ -7,21 +7,32 @@ import {Link} from 'react-router';
 import Checkbox from 'react-bootstrap/lib/Checkbox';
 import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 import Tooltip from 'react-bootstrap/lib/Tooltip';
+import HelpBlock from 'react-bootstrap/lib/HelpBlock';
 
 
 const TextField = React.createClass({
-
+    _onChange: function (e) {
+        this.props.onChange(e);
+    },
     render: function () {
         let tooltip = <Tooltip id={this.props.label}>{this.props.label}</Tooltip>;
         var label = (this.props.label != null) ? this.props.label : "";
         var name = (this.props.name != null) ? this.props.name : "";
+        var type = (this.props["data-type"] != null) ? this.props["data-type"]: undefined;
+        var len = (this.props["data-len"] != null) ? this.props["data-len"]: undefined;
+        var validationState = (this.props.errors[name]) ? 'error' : null;
+        var error = this.props.errors[name];
         return (
             <OverlayTrigger overlay={tooltip} placement="top">
-                <FormGroup>
+                <FormGroup validationState={validationState}>
                     <FormControl type="text"
                                  placeholder={label}
                                  name={name}
+                                 data-type={type}
+                                 data-len={len}
+                                 onChange={this._onChange}
                     />
+                    <HelpBlock className="warning">{error}</HelpBlock>
                     <FormControl.Feedback />
                 </FormGroup>
             </OverlayTrigger>
@@ -60,22 +71,25 @@ var SelectField = React.createClass({
     render: function () {
         var label = (this.props.label != null) ? this.props.label : "";
         var value = (this.props.value != null) ? this.props.value : "";
+        var name = (this.props.name != null) ? this.props.name : "";
+        var validationState = (this.props.errors[name]) ? 'error' : null;
+        var error = this.props.errors[name];
         var menuItems = [];
         for (var key in this.state.items) {
             menuItems.push(<option key={this.state.items[key].id}
                                    value={this.state.items[key].id}>{`${this.state.items[key].name}`}</option>);
         }
         return (
-            <FormGroup>
-
+            <FormGroup validationState={validationState}>
                 <FormControl componentClass="select"
                              onChange={this.handleChange}
                              defaultValue={value}
-                             name={this.props.name}
+                             name={name}
                 >
                     <option>{this.props.label}</option>);
                     {menuItems}
                 </FormControl>
+                <HelpBlock className="warning">{error}</HelpBlock>
             </FormGroup>
         );
     }
@@ -93,15 +107,22 @@ var InductanceParams = React.createClass({
             'fluid_type':'',
             'cooling_rating':'',
             'gas_sensor':'',
-            'fluid_level':''
-    }
+            'fluid_level':'',
+            'errors': {}
+        }
     },
 
     componentDidMount: function () {
     },
 
+    handleChange: function(e){
+        var state = this.state;
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+    },
 
     render: function () {
+        var errors = (Object.keys(this.state.errors).length) ? this.state.errors : this.props.errors;
         return (
             <div>
                 <div className="row">
@@ -110,42 +131,42 @@ var InductanceParams = React.createClass({
                             source="fluid_type"
                             label="Fluid Type"
                             name="fluid_type_id"
-                            value={this.state.fluid_type_id}/>
+                            value={this.state.fluid_type_id}
+                            errors={errors}/>
                     </div>
                     <div className="col-md-4">
                         <SelectField
                             source="fluid_level"
                             label="Fluid Level"
                             name="fluid_level_id"
-                            value={this.state.fluid_level_id}/>
+                            value={this.state.fluid_level_id}
+                            errors={errors}/>
                     </div>
                     <div className="col-md-4">
                         <SelectField
                             source="gas_sensor"
                             label="Gas Sensor"
                             name="gas_sensor_id"
-                            value={this.state.gas_sensor_id}/>
+                            value={this.state.gas_sensor_id}
+                            errors={errors}/>
                     </div>
-
                 </div>
                 <div className="row">
                     <div className="col-md-3">
                         <TextField onChange={this.handleChange}
-                                   label="Winding"
-                                   name="winding"
-                                   value={this.state.winding}/>
-                    </div>
-                    <div className="col-md-3">
-                        <TextField onChange={this.handleChange}
                                    label="Fluid Volume"
                                    name="fluid_volume"
-                                   value={this.state.fluid_volume}/>
+                                   value={this.state.fluid_volume}
+                                   errors={errors}
+                                   data-type="float"/>
                     </div>
                     <div className="col-md-3">
                         <TextField onChange={this.handleChange}
                                    label="Cooling Rating"
                                    name="cooling_rating"
-                                   value={this.state.cooling_rating}/>
+                                   value={this.state.cooling_rating}
+                                   errors={errors}
+                                   data-type="int"/>
                     </div>
                     <div className="col-md-1 ">
                         <Checkbox name="sealed" value="1"><b>Sealed</b></Checkbox>
