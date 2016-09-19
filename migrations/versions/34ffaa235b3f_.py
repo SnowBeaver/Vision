0463b1c4ea5e07671893c523b70ba4f42c578c9e
dmitry <dmitry.j@optimum-web.com>
2016-09-19 18:29:59 +0300

    Add duplicate key value handling

---
+++ migrations/versions/34ffaa235b3f_.py
@@ -0,0 +1,30 @@
+"""empty message
+
+Revision ID: 34ffaa235b3f
+Revises: 26943dd3eb2d
+Create Date: 2016-09-19 14:49:56.566141
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '34ffaa235b3f'
+down_revision = '26943dd3eb2d'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    sql = """
+        ALTER TABLE public.breaker DROP IF EXISTS frequency;
+        ALTER TABLE public.breaker DROP IF EXISTS phase_number;
+        ALTER TABLE public.breaker DROP IF EXISTS sealed;
+        ALTER TABLE public.breaker DROP IF EXISTS welded_cover;
+        ALTER TABLE public.breaker DROP IF EXISTS manufacturer_id;
+        ALTER TABLE public.tap_changer DROP IF EXISTS ltc4;
+    """
+    op.execute(sql=sql)
+
+
+def downgrade():
+    pass
