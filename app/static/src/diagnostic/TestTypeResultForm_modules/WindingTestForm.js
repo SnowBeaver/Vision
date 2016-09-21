4ad186e149c32b1cfe39a64163f23b6a70361a9d
Anna Corobco <anna@optimum-web.com>
2016-09-21 15:54:06 +0300

    Handle displaying the errors with status code > 500

--- app/static/src/diagnostic/TestTypeResultForm_modules/WindingTestForm.js
+++ app/static/src/diagnostic/TestTypeResultForm_modules/WindingTestForm.js
@@ -239,8 +239,11 @@ var WindingTestForm = React.createClass({
             message = data.responseJSON.message;
         }
         if (res.error) {
-			// Join multiple error messages
-			if (res.error instanceof Object){
+			// We get list of errors
+			if (data.status >= 500) {
+				message = res.error.join(". ");
+			} else if (res.error instanceof Object){
+				// We get object of errors with field names as key
 				for (var field in res.error) {
 					var errorMessage = res.error[field];
 					if (Array.isArray(errorMessage)) {
