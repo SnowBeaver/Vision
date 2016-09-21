a83cb3f0b9bf13f935c1d2d4e83787a248f42293
dmitry <dmitry.j@optimum-web.com>
2016-09-21 15:45:19 +0300

    Fix data after coerce doesnt change. Unify data format. Upd validation on items update

--- app/diagnostic/models.py
+++ app/diagnostic/models.py
@@ -12,7 +12,7 @@ def dump_datetime(value):
     """Deserialize datetime object into string form for JSON processing."""
     if value is None:
         return None
-    return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
+    return value.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
 
 def get_class_by_tablename(tablename):
