e7cf7197d9e56e411e0defc189d356ea701c4d8e
Anna Corobco <anna@optimum-web.com>
2016-10-03 17:58:29 +0300

    Group Test Recommendations by test type

--- app/static/src/diagnostic/TestRecommendationList.js
+++ app/static/src/diagnostic/TestRecommendationList.js
@@ -53,15 +53,72 @@ var TestRecommendation = React.createClass({
                 <td>{item.user ? item.user.name: ""}</td>
                 <td>
                     <a href="javascript:void(0)"
-                       className="glyphicon glyphicon-edit text-success"
-                       aria-hidden="true">
+                       className="btn btn-primary btn-xs">
+                        <span className="glyphicon glyphicon-pencil"> </span>
                     </a>
+                    &nbsp;
                     <a href="javascript:void(0)"
-                       className="glyphicon glyphicon-remove text-danger"
-                       aria-hidden="true">
+                       className="btn btn-danger btn-xs">
+                        <span className="glyphicon glyphicon-trash"> </span>
                     </a>
                 </td>
             </tr>
+        );
+    }
+});
+
+
+var OneTestRecommendationList = React.createClass({
+
+    handleChange: function (event, index, value) {
+        this.setState({
+            value: event.target.value
+        })
+    },
+
+    getInitialState: function () {
+        return {
+            items: [],
+            isVisible: true
+        };
+    },
+
+    edit: function () {
+        this.props.editTestForm(this.props.data.id);
+    },
+
+    render: function () {
+        var recommendations = [];
+        var testTypeId = this.props.testTypeId;
+        for (var i = 0; i < this.props.data.length; i++) {
+            var item = this.props.data[i];
+            recommendations.push(<TestRecommendation key={item.id}
+                                                     data={item}
+                                                     reloadList={this.props.reloadList}/>)
+        }
+        return (
+            <Accordion>
+                <Panel header={this.props.header}
+                       key={"recommendations" + testTypeId}
+                       eventKey={"recommendations" + testTypeId}>
+                    <Table responsive hover id="test_prof">
+                        <thead>
+                        <tr>
+                            <th className="col-md-2">Date</th>
+                            <th className="col-md-1">Code</th>
+                            <th className="col-md-2">Name</th>
+                            <th className="col-md-2">Recommendation Description</th>
+                            <th className="col-md-2">Test Recommendation Description</th>
+                            <th className="col-md-2">Created by</th>
+                            <th className="col-md-1">Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {recommendations}
+                        </tbody>
+                    </Table>
+                </Panel>
+            </Accordion>
         );
     }
 });
@@ -78,31 +135,22 @@ var TestRecommendationList = React.createClass({
     getInitialState: function () {
         return {
             recommendations: [],
-            isVisible: true,
+            isVisible: true
         };
     },
 
     componentWillReceiveProps: function (nextProps) {
         var testResultId = nextProps.testResultId;
         var testTypeId = nextProps.testTypeId;
-        if (testResultId && testTypeId &&
-            testResultId != this.props.testResultId && testTypeId != this.props.testTypeId
+        if (testResultId &&
+            testResultId != this.props.testResultId
         ) {
             this._updateList(testResultId, testTypeId);
-            //var urlParams = 'test_result_id=' + testResultId + '&test_type_id=' + testTypeId;
-            //var url = '/api/v1.0/test_recommendation/?' + urlParams;
-            //this.serverRequest = $.get(url,
-            //    function (result) {
-            //        this.setState({
-            //            recommendations: result['result']
-            //        });
-            //
-            //    }.bind(this), 'json');
         }
     },
 
-    _updateList: function (testResultId, testTypeId) {
-        var urlParams = 'test_result_id=' + testResultId + '&test_type_id=' + testTypeId;
+    _updateList: function (testResultId) {
+        var urlParams = 'test_result_id=' + testResultId;
         var url = '/api/v1.0/test_recommendation/?' + urlParams;
         this.serverRequest = $.get(url,
             function (result) {
@@ -117,41 +165,38 @@ var TestRecommendationList = React.createClass({
         this.serverRequest.abort();
     },
 
-    reloadList: function (testResultId, testTypeId) {
-        this._updateList(testResultId, testTypeId);
+    reloadList: function (testResultId) {
+        this._updateList(testResultId);
     },
 
     render: function () {
         var recommendations = [];
+        var recommendationGroups = {};
 
         for (var i = 0; i < this.state.recommendations.length; i++) {
             var item = this.state.recommendations[i];
-            recommendations.push(<TestRecommendation key={item.id}
-                                           data={item}
-                                           editTestForm={this.editTestForm}
-                                           reloadList={this.props.reloadList}/>)
+            if (!recommendationGroups[item.test_type_id]) {
+                recommendationGroups[item.test_type_id] = [];
+            }
+            recommendationGroups[item.test_type_id].push(item);
         }
+
+        for (var i in recommendationGroups) {
+            recommendations.push(<OneTestRecommendationList key={i}
+                                                            testTypeId={i}
+                                                            data={recommendationGroups[i]}
+                                                            reloadList={this.props.reloadList}
+                                                            header={recommendationGroups[i][0].test_type.name}/>)
+        }
+
         return (
                 <div>
                     <div className="row">
                         <Accordion>
-                            <Panel header="Recommendations" key="recommendations" eventKey="recommendations">
-                                <Table responsive hover id="test_prof">
-                                    <thead>
-                                    <tr>
-                                        <th className="col-md-2">Date</th>
-                                        <th className="col-md-1">Code</th>
-                                        <th className="col-md-2">Name</th>
-                                        <th className="col-md-2">Recommendation Description</th>
-                                        <th className="col-md-2">Test Recommendation Description</th>
-                                        <th className="col-md-2">Created by</th>
-                                        <th className="col-md-1">Actions</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    {recommendations}
-                                    </tbody>
-                                </Table>
+                            <Panel header="Recommendations"
+                                   key="recommendationsBlock"
+                                   eventKey="recommendationsBlock">
+                                {recommendations}
                             </Panel>
                         </Accordion>
                     </div>
