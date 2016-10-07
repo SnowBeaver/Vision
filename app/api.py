a09e5c60c55bcd5b9c66e13dc83a8049fc03ad90
Anna Corobco <anna@optimum-web.com>
2016-10-07 10:37:30 +0300

    schedule/multi endpoint: Saving or updating multiple tasks

--- app/api.py
+++ app/api.py
@@ -281,7 +281,7 @@ def add_items(path, data):
     return [add_item(path, {'campaign_id':campaign_id, 'equipment_id':id}).id for id in equipment_ids]
 
 
-def add_or_update_tests(path):
+def add_or_update_items(path):
     items_model = get_model_by_path(path)
     items = []
     for test in request.json:
@@ -528,7 +528,7 @@ def handler_tests(path):
                     'winding_test'):
         abort(404)
     abort_if_json_missing()
-    return return_json('result', add_or_update_tests(path))
+    return return_json('result', add_or_update_items(path))
 
 
 # Duplicate test result and related electrical or fluid profile
@@ -596,5 +596,12 @@ def create_test_repair_note_handler():
     new_item = add_user_id_and_save_item(path, validated_data)
     return return_json('result', new_item.id)
 
+
+# Create or update a lot of tasks
+@api_blueprint.route('/schedule/multi/', methods=['POST'])
+def handler_tasks():
+    path = 'schedule'
+    abort_if_json_missing()
+    return return_json('result', add_or_update_items(path))
 
 api.register_blueprint(api_blueprint)
