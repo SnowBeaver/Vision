170b6381052cc0907126c6c4647f37e415379676
dmitry <dmitry.j@optimum-web.com>
2016-10-12 17:38:42 +0300

    Fix url in test_api

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,7 @@ sys.path.append('..')
 from app.api_utility import model_dict
 
 
-url = 'http://dev.vision.local:8001/api/v1.0/{path}/'
+url = 'http://localhost:8001/api/v1.0/{path}/'
 user = 'test'
 password = 'test-123'
 path_to_get_token = 'token'
