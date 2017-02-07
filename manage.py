2ae3e8cbb3acc797011a9bcf01b6f54fde815635
Anna Corobco <anna@optimum-web.com>
2017-02-07 18:36:27 +0200

    Import equipment, manufacturers, location, physic norms from mdb file

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
+from app.data_import.import_perception import run_import
+
 app = Flask(__name__)
 app.config.from_object('config')
 
@@ -12,7 +14,7 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
-
+manager.command(run_import)
 
 if __name__ == '__main__':
     manager.run()
